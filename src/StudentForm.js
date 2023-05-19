b67d09fa759483e3a1c78b153904d98f1b97f642
tjupp <travisjupp@gmail.com>
2023-05-18 21:20:17 -0600

    change for to htmlFor

--- src/StudentForm.js
+++ src/StudentForm.js
@@ -29,42 +29,42 @@ function StudentForm() {
             <section>
                 <h1>Basic React form example: Student Admin</h1>
                 <form onSubmit={handleSubmit} id="studentAdminForm" className={styles}>
-                    <label for="firstName">First Name: {data.firstName}</label>
+                    <label htmlFor="firstName">First Name: {data.firstName}</label>
                     <input
                         type="text"
                         id="firstName"
                         value={data.firstName}
                         onChange={handleChange}
                     />
-                    <label for="lastName">Last Name: {data.lastName}</label>
+                    <label htmlFor="lastName">Last Name: {data.lastName}</label>
                     <input
                         type="text"
                         id="lastName"
                         value={data.lastName}
                         onChange={handleChange}
                     />
-                    <label for="age">Age: {data.age}</label>
+                    <label htmlFor="age">Age: {data.age}</label>
                     <input
                         type="number"
                         id="age"
                         value={data.age}
                         onChange={handleChange}
                     />
-                    <label for="address">Address: {data.address}</label>
+                    <label htmlFor="address">Address: {data.address}</label>
                     <input
                         type="text"
                         id="address"
                         value={data.address}
                         onChange={handleChange}
                     />
-                    <label for="homeroom">Homeroom: {data.homeroom}</label>
+                    <label htmlFor="homeroom">Homeroom: {data.homeroom}</label>
                     <input
                         type="number"
                         id="homeroom"
                         value={data.homeroom}
                         onChange={handleChange}
                     />
-                    <label for="studentID">Student ID: {data.studentID}</label>
+                    <label htmlFor="studentID">Student ID: {data.studentID}</label>
                     <input
                         type="text"
                         id="studentID"
@@ -81,7 +81,7 @@ function StudentForm() {
                             value="fish"
                             onChange={handleChange}
                         />
-                        <label for="fish">Fish</label>
+                        <label htmlFor="fish">Fish</label>
                         <input
                             type="radio"
                             name="lunchOp"
@@ -89,7 +89,7 @@ function StudentForm() {
                             value="beef"
                             onChange={handleChange}
                         />
-                        <label for="beef">Beef</label>
+                        <label htmlFor="beef">Beef</label>
                     </fieldset>
                     <input type="submit" id="submit" value="Submit" />
                 </form>
